862efd3ff17dd76d8f0d2fdee13a05de31468de5
Dylan Tientcheu <dylantientcheu@gmail.com>
2022-07-11 16:25:57 +0200

    chore: :lipstick: update file sources

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './fireman'
+import * as firestoreFns from "./firestore"
+
+const fsHelper = firestoreFns;
+// const fsAdminHelper = {}; // coming soon
+// const rtdbHelper = {}; // coming soon
+// const authHelper = {}; // coming soon
+
+export { fsHelper };
